e8367362991ffa312b54ddea9b5482fdf6208798
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2015-08-30 18:44:44 +0200

    update the docker build script to reflect the new files

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker build -t 'dashi:nginx' --rm=true --no-cache=true -f Dockerfile-nginx .
-docker build -t 'dashi:web' --rm=true --no-cache=true -f Dockerfile-web .
+docker build -t 'dashi:web' --rm=true --no-cache=true -f Dockerfile .
